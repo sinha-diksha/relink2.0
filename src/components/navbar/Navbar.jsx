02ed7891a8a9b2e95f89e2279053b415af444d3f
diksha sinha <diksha@dikshas-MacBook-Air.local>
2024-04-28 22:11:40 +0530

    code added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,13 @@ import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { AuthContext } from "../../context/authContext";
 const Navbar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext);
+
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="left">
@@ -29,11 +35,8 @@ const Navbar = () => {
         <EmailOutlinedIcon />
         <NotificationsOutlinedIcon />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <span>John Doe</span>
+          <img src={currentUser.profilePic} />
+          <span>{currentUser.name}</span>
         </div>
       </div>
     </div>
